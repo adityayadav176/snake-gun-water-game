b7872f71bfe758f26605e9a5d7a6edaf8d240beb
adityayadav176 <ay6598aditya@gmail.com>
2026-02-20 12:12:23 +0530

    Add new game water snake gun

---
+++ index.js
@@ -0,0 +1,40 @@
+const gameBtn = document.getElementById("gameBtn");
+const drawnF = document.getElementById("Drawn");
+const ScoreF = document.getElementById("Score");
+const loseF = document.getElementById("lose");
+
+
+
+gameBtn.addEventListener("click", () => {
+    let arr = ["snake", "water", "gun"];
+    let score = 0;
+    let drawn = 0;
+    let Moves = 10;
+    let beat = 0;
+
+    while (Moves > 0) {
+        // INPUTS
+
+        let UserInput = prompt("First user choice (snake, water, gun)");
+        let ComputerInput = arr[Math.floor(Math.random() * arr.length)];
+
+        UserInput = UserInput.toLowerCase().trim();
+
+        // 1️⃣ Check invalid input
+        if (!arr.includes(UserInput) || !arr.includes(ComputerInput)) {
+            alert("Invalid input!");
+            continue;
+        }
+
+      
+
+      
+        
+        Moves--;
+        drawnF.innerText = drawn;
+        ScoreF.innerText = score;
+        loseF.innerText = beat;
+    }
+
+});
+
